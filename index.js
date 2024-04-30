d6b7109c2d54d8f181755052663c8b3f8299f3e7
JongDeug <jongdeug2021@gmail.com>
2024-04-30 18:20:59 +0900

    express, express-handlebars 세팅

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
+// require('dotenv').config();
+// const { MongoClient } = require('mongodb');
+//
+// const uri = `mongodb://${process.env['DB_ID']}:${process.env['DB_PASS']}@localhost:27017/board`;
+// const client = new MongoClient(uri, { authSource: 'admin' });
+//
+// async function run() {
+//     try {
+//         const board = client.db('board').collection('test');
+//         await board.insertOne({ name: '파묘' });
+//         const movie = await board.findOne({ name: '파묘' });
+//         console.log(movie);
+//     } finally {
+//         await client.close();
+//     }
+// }
+//
+// run().catch(console.dir);
+const express = require('express');
+const handlebars = require('express-handlebars');
+const app = express();
 
-const uri = `mongodb://${process.env['DB_ID']}:${process.env['DB_PASS']}@localhost:27017/board`;
-const client = new MongoClient(uri, { authSource: 'admin' });
+app.engine('handlebars', handlebars.engine()); // 템플릿 엔진 등록
+app.set('view engine', 'handlebars'); // 웹 페이지 로드시 사용할 템플릿 엔진 등록
+app.set('views', __dirname + '/views'); // 뷰 디렉터리 설정
 
-async function run() {
-    try {
-        const board = client.db('board').collection('test');
-        await board.insertOne({ name: '파묘' });
-        const movie = await board.findOne({ name: '파묘' });
-        console.log(movie);
-    } finally {
-        await client.close();
-    }
-}
+app.get('/', (req, res) => {
+    // views.home.handlebars 파일에 데이터를 렌더링
+    res.render('home', { title: '안녕하세요', message: '만나서 반갑습니다!' });
+});q
 
-run().catch(console.dir);
+app.listen(3000);
